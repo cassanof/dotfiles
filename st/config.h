58337475bba6280485f15cf5a2ff39a6ad673baf
elleven@lakehouse <elleven@lakehouse>
2020-07-05 20:11:31 +0200

    adjusted st opacity

--- st/config.h
+++ st/config.h
@@ -108,7 +108,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 2;
 
 /* bg opacity */
-float alpha = 0.75;
+float alpha = 0.95;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
