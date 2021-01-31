c2c0744cf7b1ef72f8465d040e3c6f90c45f1fe2
elleven11 <elleven11@github.com>
2021-01-31 15:23:24 -0500

    added dmenu and some other stuff

---
+++ dmenu/README.md
@@ -0,0 +1,2 @@
+# My dmenu config
+I usually use it with j4-dmenu-desktop
---
+++ dmenu/config.h
@@ -0,0 +1,23 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+  "Inconsolata Nerd Font Mono:pixelsize=22:antialias=true:autohint=true"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#FF4D4D" },
+	[SchemeOut] = { "#000000", "#F80D0D" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
