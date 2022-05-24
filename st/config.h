3f19f3a4be6b6f85fe35a2d862ca56d78ac04ded
elleven11 <federico.cassano@federico.codes>
2022-05-23 21:08:28 -0400

    disables buttons

--- st/config.h
+++ st/config.h
@@ -218,8 +218,8 @@ ResourcePref resources[] = {
  */
 static MouseShortcut mshortcuts[] = {
 	/* button               mask            string */
-	{ Button4,              XK_NO_MOD,      "\031" },
-	{ Button5,              XK_NO_MOD,      "\005" },
+	// { Button4,              XK_NO_MOD,      "\031" },
+	// { Button5,              XK_NO_MOD,      "\005" },
 };
 
 /* Internal keyboard shortcuts. */
