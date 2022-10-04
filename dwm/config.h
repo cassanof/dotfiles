eb92156703742f8ac435753371a81854517ec6ce
elleven11 <federico.cassano@federico.codes>
2022-10-04 09:15:08 -0400

    forgot keys

--- dwm/config.h
+++ dwm/config.h
@@ -118,6 +118,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
 	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
