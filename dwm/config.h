2c07f87b29f7cd8c8b95666a56d7950193bd731b
cassanof <federico.cassano@federico.codes>
2024-07-10 21:42:32 -0700

    more tags!!!

--- dwm/config.h
+++ dwm/config.h
@@ -18,7 +18,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
         /* xprop(1):
@@ -120,6 +120,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_5,                      4)
 	TAGKEYS(                        XK_6,                      5)
 	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      6)
+	TAGKEYS(                        XK_9,                      6)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
